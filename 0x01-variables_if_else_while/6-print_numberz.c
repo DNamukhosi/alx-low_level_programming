fdd0640b3df2642343224dc26e737fc319edb0c4
DNamukhosi <damarisnamukosi@gmail.com>
2023-06-18 11:23:55 -0700

    a program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print tall single numbers of base 10
+ *
+ * Description: print all the single number of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int num;
+
+        for (num = 0; num <= 9; num++)
+		putchar(num + '0');
+	putchar('\n');
+        return (0);
+}
